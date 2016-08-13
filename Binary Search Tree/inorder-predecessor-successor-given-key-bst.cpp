7b3d50af9b9b7ab12787f77439b43c48a8389d5b
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-08-13 23:05:35 +0530

    commit

---
+++ Binary Search Tree/inorder-predecessor-successor-given-key-bst.cpp
@@ -0,0 +1,8 @@
+// http://www.geeksforgeeks.org/inorder-predecessor-successor-given-key-bst/
+// Inorder predecessor and successor for a given key in BST
+
+/*
+You need to find the inorder successor and predecessor of a given key. In case the given key is not found in BST,
+then return the two values within which this key will lie. Following is the algorithm to reach the desired result. 
+*/
+
---
+++ Binary Tree/iterative-preorder-traversal.cpp
@@ -0,0 +1,4 @@
+// http://www.geeksforgeeks.org/iterative-preorder-traversal/
+// Iterative Preorder Traversal
+// InterviewBit Entry := https://www.interviewbit.com/problems/inorder-traversal/
+// My solution := https://github.com/sidgupta234/InterviewBit/blob/master/Level-6/Trees/preorderTraversal.cpp
